2db871765cc354057321ddc0a26ac3eac53f3c68
Greg Kessman <greg.kessman@gmail.com>
2018-07-14 08:57:19 -0400

    Initial commit - trivia 1.0

---
+++ lambda/custom/questions.js
@@ -0,0 +1,251 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  
+  QUESTIONS_EN_US: [
+    {
+      'Which version of the drug Mentats gives players a plus 5 Intelligence increase?': [
+        'Berry Mentats',
+        'Grape Mentats',
+        'Orange Mentats',
+        'None'
+      ],
+    },
+    {
+      'In what location can players find the Strength Vault-Tec Bobblehead?': [
+        'Mass Fusion Building',
+        'Trinity Tower',
+        'Corvega Assembly Plant',
+        'Atom Cats Garage'
+      ],
+    },
+    {
+      'Which settlement utilized the G.O.A.T. testing system before letting the player enter?': [
+        'Covenant',
+        'Finch Farm',
+        'Bunker Hill',
+        'Sunshine Tidings Co-op',
+      ],
+    },
+    {
+      'On the Brotherhood of Steel\'s Zepplin "The Prydwen," what is the name of the person who asks the player to retrieve Technical Documents?': [
+        'Quinlan',
+        'Haylen',
+        'Cade',
+        'Teagan',
+      ],
+    },
+    {
+      'How many uniquely named robots reside at the General Atomics Location in the Wasteland?': [
+        '17',
+        '6',
+        '12',
+        '20',
+      ],
+    },
+    {
+      'Which Pip-Boy 3000 Mark 4 minigame is inspired by the classic arcade title Donkey Kong?': [
+        'Red Menace',
+        'Captain Cosmos',
+        'Zeta Invaders',
+        'Grognak and The Ruby Ruins',
+      ],
+    },
+    {
+      'How many new species of mutated enemies did Far Harbor introduce into Fallout 4?': [
+        '5',
+        '4',
+        '7',
+        '6',
+      ],
+    },
+    {
+      'What is the name of the legendary weapon you can obtain at the Rook Family House after completing the quest "Gun Run"?': [
+        'Reba 2',
+        'Justice',
+        'Old Reliable',
+        'Tinker Tom Special',
+      ],
+    },
+    {
+      'Which building\'s roof does the U.S.S. Constitution crash land on at the end of Captain Ironsides\' questline?': [
+        'Weatherby Savings & Loan',
+        'Trinity Tower',
+        'HalluciGen, Inc.',
+        'Layton Towers',
+      ],
+    },
+    {
+      'Which Fallout 4 enemy is described as the "fiercest flying insect in the commonwealth"?': [
+        'Stingwings',
+        'Bloatflies',
+        'Bloodbugs',
+        'Flying Soldier Ants',
+      ],
+    },
+    {
+      'Reading what magazine gives players plus 5 Radiation Resistance?': [
+        'Giant Insects Invade',
+        'Rise of the Radiated',
+        'Invasion of the Zetans',
+        'Rise of the Mutants',
+      ],
+    },
+    {
+      'Which of these robots does not compete in the race at the Easy City Downs track?': [
+        'Tin Man',
+        'Atomic Dreamz',
+        'Bob\'s Your Uncle',
+        'Ol\' Rusty',
+      ],
+    },
+    {
+      'Which Fallout Four companion does not require the player to romance them in order for their unique perk to be active?': [
+        'Deacon',
+        'Cait',
+        'Curie',
+        'John Hancock',
+      ],
+    },
+    {
+      'Of all the Fallout Four perks, which one features an image of Vault Boy and a man with his arm shot off in the Perk Menu?': [
+        'Critical Banker',
+        'Quick Hands',
+        'Penetrator',
+        'Gun-Fu',
+      ],
+    },
+    {
+      'Where can the ghoulified Vault-Tec Representative be found in the Commonwealth?': [
+        'Hotel Rexford',
+        'Parkview Apartments',
+        'Diamond City',
+        'Layton Towers',
+      ],
+    },
+    {
+      'Who is the leader of the Minutemen?': [
+        'Preston Garvey',
+        'Robert MacReady',
+        'Brother Thomas',
+        'John Hancock',
+      ],
+    },
+    {
+      'When engaged in combat, what are the Minutemen\'s weapon of choice?': [
+        'Laser Musket',
+        'Pipe Rifle',
+        'Laser Rifle',
+        'Fat Man',
+      ],
+    },
+    {
+      'Which location in the commonwealth do you first interact with the Brotherhood of Steel?': [
+        'Cambridge Police Station',
+        'Corvega Assembly Plant',
+        'East Boston Police Station',
+        'National Guard Training Yard',
+      ],
+    },
+    {
+      'What is the name of the town your character lives in before the Great War?': [
+        'Sanctuary Hills',
+        'Tenpines Bluff',
+        'Concord',
+        'Nordhagen Beach',
+      ],
+    },
+    {
+      'Which vault are you rushing to enter prior to the bombs dropping?': [
+        'Vault 111',
+        'Vault 75',
+        'Vault 95',
+        'Vault 114',
+      ],
+    },
+    {
+      'How many vaults exist in the commonwealth?': [
+        '5',
+        '7',
+        '9',
+        '8',
+      ],
+    },
+    {
+      'Which organization is responsible for creating synths?': [
+        'The Institute',
+        'The Railroad',
+        'The Brotherhood of Steel',
+        'The Minutemen',
+      ],
+    },
+    {
+      'How many generations of synths were created?': [
+        '3',
+        '4',
+        '5',
+        '2',
+      ],
+    },
+    {
+      'Who is the Mayor of the town Goodneighbor?': [
+        'John Hancock',
+        'Robert MacReady',
+        'Deacon',
+        'Cait',
+      ],
+    },
+    {
+      'What armor modification do you receive after completing the Brotherhood of Steel quest line?': [
+        'Jetpack',
+        'Medic Pump',
+        'Anti-Radiation',
+        'Portable Nuke Launcher',
+      ],
+    },
+    {
+      'Which song was used the most during Fallout Four advertising?': [
+        'The Wanderer',
+        'Atom Bomb Baby',
+        'I don\'t Want To Set The World On Fire',
+        'Uranium Fever',
+      ],
+    },
+    {
+      'What is the name of the scientist you must look for to help you gain access to the Institute?': [
+        'Brian Virgil',
+        'John McDonough',
+        'Jack Spruce',
+        'John Codman',
+      ],
+    },
+    {
+      'Where do you first find the Minutemen, in which you have to help them escape?': [
+        'Museum of Freedom',
+        'Fraternal Post 115',
+        'Boston Public Library',
+        'Lexington',
+      ],
+    },
+    {
+      'What year do you finally emerge from your vault after the Great War?': [
+        'Year 2287',
+        'Year 2277',
+        'Year 2180',
+        'Year 2301',
+      ],
+    },
+    {
+      'What is the strongest non-legendary Deathclaw you can encounter?': [
+        'Mythic Deathclaw',
+        'Albino Deathclaw',
+        'Savage Deathclaw',
+        'Deathclaw Matriarch', 
+      ],
+    },
+  ]
+};
